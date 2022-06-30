2f72751cd8cacb3ef2ed3150bb478bc19c628ed7
CywaZaxthru <cywazxthru@gmail.com>
2022-06-30 12:13:16 +0200

    Added account creation date

--- steamAPI.js
+++ steamAPI.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const { getAllByPlaceholderText } = require('@testing-library/react');
 const fetch = require('node-fetch');
 
 async function callSteamAPI(id){
@@ -10,6 +11,7 @@ async function callSteamAPI(id){
     }
     steam.ID32 = Number(steam.ID64.substr(-16,16)) - 6561197960265728
     Object.assign(steam, await getStats(steam.ID64))
+    Object.assign(steam, await getDate(steam.ID64))
     return steam
 }
 
@@ -54,6 +56,64 @@ async function getStats(id){
             wr: Math.round((won/matches)*100)/100,
             matches: matches,
             frags: Math.round((kills/matches)*100)/100,
+        })
+    })
+    return response
+}
+
+async function getDate(ID){
+    const KEY = process.env.Steam_API_KEY;
+    const URL = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2?key=${KEY}&steamids=${ID}`
+    const response = await fetch(URL)
+    .then(res => res.json())
+    .then(res => {
+        let date = new Date(res.response.players[0].timecreated*1000).toString()
+        date = date.split(" ")
+        date = [date[2], date[1], date[3]]
+
+        switch(date[1]){
+            case "Jan":
+                date[1] = "January"
+                break;
+            case "Feb":
+                date[1] = "February"
+                break;
+            case "Mar":
+                date[1] = "March"
+                break;
+            case "Apr":
+                date[1] = "April"
+                break;
+            case "May":
+                date[1] = "May"
+                break;
+            case "Jun":
+                date[1] = "June"
+                break;
+            case "Jul":
+                date[1] = "July"
+                break;
+            case "Aug":
+                date[1] = "August"
+                break;
+            case "Sep":
+                date[1] = "September"
+                break;
+            case "Oct":
+                date[1] = "October"
+                break;
+            case "Nov":
+                date[1] = "November"
+                break;
+            case "Dec":
+                date[1] = "December" 
+                break;
+        }
+
+        date = date.join(" ")
+
+        return({
+            creationDate: date
         })
     })
     return response
